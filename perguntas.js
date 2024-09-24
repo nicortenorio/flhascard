bb12853d444a054fc910c77eff89be4ba11e5296
nicortenorio <132095089+nicortenorio@users.noreply.github.com>
2024-09-24 16:27:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'conhecimentos gerais',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    ' Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'Filosofia',
+' De quem é a famosa frase “Penso, logo existo”??',
+    'Descartes.'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    
+    'João Goulart.'
+)
